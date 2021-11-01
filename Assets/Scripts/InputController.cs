ede20203a69798c6972d8a93f625407b31f4d51a
Malan-Tai <malantai99@gmail.com>
2021-11-01 17:46:18 +0100

    add pause menu

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -11,6 +11,9 @@ public class InputController : MonoBehaviour
     [SerializeField]
     private float _maxRunSpeed;
 
+    [SerializeField]
+    private GameObject _pauseMenu;
+
     private void Start()
     {
         _dynamicPlayer = GetComponent<DynamicPlayer>();
@@ -20,29 +23,37 @@ public class InputController : MonoBehaviour
     {
         if (_dynamicPlayer == null) return;
 
-        if (!_dynamicPlayer.IsDashing && !_dynamicPlayer.IsWallJumping)
+        if (!_pauseMenu.activeInHierarchy)
         {
-            if (_dynamicPlayer.IsRunning)
+            if (!_dynamicPlayer.IsDashing && !_dynamicPlayer.IsWallJumping)
             {
-                _dynamicPlayer.SetHorizontalSpeed(_maxRunSpeed * Input.GetAxis("Horizontal"));
-            }
-            else
-            {
-                _dynamicPlayer.SetHorizontalSpeed(_maxSpeed * Input.GetAxis("Horizontal"));
+                if (_dynamicPlayer.IsRunning)
+                {
+                    _dynamicPlayer.SetHorizontalSpeed(_maxRunSpeed * Input.GetAxis("Horizontal"));
+                }
+                else
+                {
+                    _dynamicPlayer.SetHorizontalSpeed(_maxSpeed * Input.GetAxis("Horizontal"));
+                }
+
+                if (Input.GetAxis("Vertical") < -0.5f)
+                {
+                    _dynamicPlayer.TryGettingDownThinPlatform();
+                }
             }
 
-            if (Input.GetAxis("Vertical") < -0.5f)
-            {
-                _dynamicPlayer.TryGettingDownThinPlatform();
-            }
+            if (Input.GetButtonDown("Jump") && (_dynamicPlayer.CanJump)) _dynamicPlayer.Jump();
+            if (Input.GetButtonUp("Jump")) _dynamicPlayer.RestoreGravity();
+
+            if (Input.GetButtonDown("Dash")) _dynamicPlayer.Dash(Input.GetAxis("Horizontal"));
+
+            if (Input.GetButtonDown("Run") && (_dynamicPlayer.Velocity.y == 0)) _dynamicPlayer.StartRunning();
+            if (Input.GetButtonUp("Run")) _dynamicPlayer.StopRunning();
         }
-        
-        if (Input.GetButtonDown("Jump") && (_dynamicPlayer.CanJump)) _dynamicPlayer.Jump();
-        if (Input.GetButtonUp("Jump")) _dynamicPlayer.RestoreGravity();
 
-        if (Input.GetButtonDown("Dash")) _dynamicPlayer.Dash(Input.GetAxis("Horizontal"));
-
-        if (Input.GetButtonDown("Run") && (_dynamicPlayer.Velocity.y == 0)) _dynamicPlayer.StartRunning();
-        if (Input.GetButtonUp("Run")) _dynamicPlayer.StopRunning();
+        if (Input.GetButtonDown("Pause"))
+        {
+            _pauseMenu.SetActive(!_pauseMenu.activeInHierarchy);
+        }
     }
 }
